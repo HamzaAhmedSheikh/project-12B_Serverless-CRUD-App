b8bba601beb6824e34e2878a8c814a115cc5612d
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-11-25 02:46:46 +0500

    I have add the css in app.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,22 @@ import { TextField, Modal, AppBar, Toolbar, Typography } from "@material-ui/core
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
+ function randomId() {
+  return Math.round(Math.random() * 20) - 10;
+ }
+
+ function getModalStyle() {
+  const top = 50 + randomId();
+  const left = 50 + randomId();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -30,7 +46,39 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function Home() {
   const classes = useStyles();
+  const [data, setData] = useState<null | Data[]>();
+  const [fetchData, setFetchData] = useState(false);
+  const [openCreate, setOpenCreate] = useState(false);
+  const [openUpdate, setOpenUpdate] = useState(false);
+
+  interface Data {
+    ref: object;
+    ts: number;
+    data: {
+      message: string;
+    };
+  }
+
+  // create modal functions
+
+  const handleOpenCreate = () => {
+    setOpenCreate(true);
+  };
+
+  const handleCloseCreate = () => {
+    setOpenCreate(false);
+  };
+
+  // updated modal functions
   
+  const handleOpenUpdated = () => {
+    setOpenUpdate(true);
+  };
+
+  const handleCloseUpdated = () => {
+    setOpenUpdate(false);
+  };
+
   return (
    <div className='main'>
     <AppBar position="static" color='secondary'>
